560d9b915ff780733ff5c3fb46ccc8a364c123ad
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2023-12-27 20:12:19 +0200

    printfv1.1

--- _printf.c
+++ _printf.c
@@ -2,59 +2,59 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int args_number = 0;
-    int i;
-    char one_character;
-    char *str;
+va_list args;
+int args_number = 0;
+int i;
+char one_character;
+char *str;
 
-    va_start(args, format);
+va_start(args, format);
 
-    if (format == NULL)
-    {
-        return (-1);
-    }
+if (format == NULL)
+{
+return (-1);
+}
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-           switch (*(++format))
-           {
-           case 'c':
-            one_character = va_arg(args, int);
-            _putchar(one_character);
-            break;
-           case 's':
-            str = va_arg(args, char*);
-            if (str != NULL)
-            {
-                for (i = 0; str[i] != '\0'; i++)
-                {
-                    _putchar(str[i]);
-                }
-            }
-            else
-            {
-                _putchar(' ');
-            }
-            break;
-           case '%':
-            _putchar('%');
-            break;
-           default:
-            _putchar('?');
-            break;
-           } 
-        }
-        else
-        {
-            _putchar(*format);
-        }
-        format++;
-        args_number++;
-    }
+while (*format)
+{
+if (*format == '%')
+{
+switch (*(++format))
+{
+case 'c':
+one_character = va_arg(args, int);
+_putchar(one_character);
+break;
+case 's':
+str = va_arg(args, char*);
+if (str != NULL)
+{
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+{
+_putchar(' ');
+}
+break;
+case '%':
+_putchar('%');
+break;
+default:
+_putchar('?');
+break;
+}
+}
+else
+{
+_putchar(*format);
+}
+format++;
+args_number++;
+}
 
-    va_end(args);
-    return args_number;
+va_end(args);
+return (args_number);
 }
